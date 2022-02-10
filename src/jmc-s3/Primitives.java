23ff5f95a5dd9b93962ae3f4aa7ea5ded4a134ba
Akosh Poemandres <arjay.jones@nlucis.dev>
2022-02-09 19:58:18 -0800

    added some stuff for probing methods on primitives

---
+++ src/jmc-s3/Primitives.java
@@ -0,0 +1,14 @@
+/*
+ * Exploring Primitive Types  
+ */
+public class Primitives {
+
+  // Numerical 
+  static int one = 1;
+  static int two = 2;
+  static int numRed = 3, 
+             numBlue = 4;
+  public static void main(String[] args) {
+    System.out.println(one);
+  }
+}
